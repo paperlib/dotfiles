992bc382be3c4ad995c3faece7f13ae795436a97
stephane <stephane@paperlib.com>
2024-09-29 20:39:20 +0200

    add missing enclosing color sequence escape sequences have to be enclosed in \[\033[ and \]

--- .profile.git-prompt.sh
+++ .profile.git-prompt.sh
@@ -3,9 +3,11 @@
 . /usr/local/git/contrib/completion/git-prompt.sh
 
 # -- https://robotmoon.com/256-colors
-COLOR_GIT_SIGN='\[\033[01;38;5;230m'
-COLOR_GIT_BRANCH='\[\033[01;38;5;028m'
-COLOR_GIT_DIRTY='\[\033[01;38;5;215m'
+# -- https://tldp.org/HOWTO/Bash-Prompt-HOWTO/x329.html
+# -- https://zipcon.net/~swhite/docs/computers/linux/shell_prompts.html
+COLOR_GIT_SIGN='\[\033[01;38;5;230m\]'
+COLOR_GIT_BRANCH='\[\033[01;38;5;028m\]'
+COLOR_GIT_DIRTY='\[\033[01;38;5;215m\]'
 COLOR_RESET='\[\033[0m\]'
 
 GIT_PS1_SHOWDIRTYSTATE=true
